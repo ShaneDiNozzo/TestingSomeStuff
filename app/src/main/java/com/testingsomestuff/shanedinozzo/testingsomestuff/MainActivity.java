2ae42124e4e022d5d06fa3ed89c5fdc872ce503b
ShaneDiNozzo <shanedinozzo@gmail.com>
2014-11-10 10:12:13 +0100

    Add toast message on button click and add more words to my own dictionary

--- app/src/main/java/com/testingsomestuff/shanedinozzo/testingsomestuff/MainActivity.java
+++ app/src/main/java/com/testingsomestuff/shanedinozzo/testingsomestuff/MainActivity.java
@@ -1,7 +1,10 @@
 package com.testingsomestuff.shanedinozzo.testingsomestuff;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void _gombNyomas(View view) {
+        Context context = getApplicationContext();
+        Toast.makeText(context, "Megnyomtad a gombot", Toast.LENGTH_SHORT).show();
     }
 }
